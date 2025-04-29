4b8437609915dbec9b43da9d5d43ca2140880324
sphirebluela <laura.sphireblue@gmail.com>
2025-04-29 11:41:05 -0700

    conditional logic option writing
    different ways to do conditional objects

--- vite-project/src/components/products/index.jsx
+++ vite-project/src/components/products/index.jsx
@@ -2,11 +2,26 @@ import ProductItem from "./components/product-item";
 import "./style.css";
 
 function ProductList({ listOfProducts }) {
-  //dummyProductData){{
+  const flag = true; // This can be a prop or state variable to toggle the heading
+
+  //function renderTextBlock(getFlag) {
+  //  return getFlag ? <h3>Heading Three</h3> : <h4>Heading Four</h4>;
+  //}
+
+  //const renderTextBlock = flag ? <h3>Heading Three</h3> : <h4>Heading Four</h4>;
+
+  let renderTextBlock = null;
+  if (flag) {
+    renderTextBlock = <h3>Heading Three</h3>;
+  } else {
+    renderTextBlock = <h4>Heading Four</h4>;
+  }
+
   return (
     <div>
       <h3 className="title">ECommerce Project</h3>
       {/*<ProductItem />*/}
+      {renderTextBlock}
       <ul>
         {listOfProducts.map((item, index) => (
           <ProductItem singleProductItem={item} key={index}></ProductItem>
